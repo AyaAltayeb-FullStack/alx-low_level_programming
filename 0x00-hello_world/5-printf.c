46b216b3ad66d571f0cdcd2695e8ed10ab7cfc62
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2022-10-17 16:18:32 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
-
-
+/**
+ *
+ * * main - Entry point
+ *
+ * *   print a string to a screen 
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0)
 }
