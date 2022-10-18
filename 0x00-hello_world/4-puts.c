2056e1041e8d61660953d3cf9892eb40928570b9
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2022-10-17 17:17:37 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+/**
+ * main - entry point
+ *
+ * puts something to screen
+ * Return:0 (success)
+ */
 int main()
 {
-	puts("Programming is like building a multilingual puzzle/n");
-	return(0);
+puts("Programming is like building a multilingual puzzle\n");
+return(0);
 }
