60f29a9a00e46768851205dfdb4342eb0b663aad
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2022-10-25 15:42:53 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swap two inters
+ * @a: pointer to the firdt integer
+ * @b: pointer to the second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+int t = *a;
+a* = *b;
+*b = t;
+}
