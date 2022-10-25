6b6ff0555af92019c6388394a3c96111c2c17b47
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2022-10-25 15:32:17 -0700

    all

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-
 /**
  * reset_to_98 -  updates a value its point to 
  * @n: pointer to an int 
  *
  * Return: nothing
  */
-
-void reset_to_98(int *n){
-*n=98;
+void reset_to_98(int *n)
+{
+*n = 98;
 }
