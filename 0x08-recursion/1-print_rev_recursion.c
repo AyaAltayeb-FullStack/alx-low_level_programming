b42acc6f800b3342841953e386e84cc0a380064b
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2023-10-25 18:24:59 -0700

    update 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
-  * _puts_recursion - prints a string followed by a line
-  * @s: pointer to a string
-  * Return: nothing
-  **/
-void _puts_recursion(char *s)
+ * _print_rev_recursion -  prints a string in reverse order
+ * @s: pointer to string
+ * Return: nothing
+ **/
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
