ca3765fcbec4b466d0e0da024dec440f3d51521a
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2023-10-25 18:16:01 -0700

    update the  betty codeing style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _put_recursion - using the recursion a string is printed followed by a new line
+ * _put_recursion - a string is printed followed by a new line
  * @s: pointer to a string
  * Return: nothing
- * */
+ **/
 
 void _puts_recursion(char *s)
 {
-	if(*s =='\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
-	_puts_recursion(s+1);
+	_puts_recursion(s + 1);
 }
